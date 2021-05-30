a38e44941d69714eab586f754c51c2391e89cb30
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-05-30 00:39:43 -0500

    app

--- climate_app.py
+++
@@ -1,9 +0,0 @@
-# Imports Dependencies
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from flask import Flask, jsonify
-from sqlalchemy import create_engine, func
-import sqlalchemy
-
-import datetime as dt
-import numpy as np
