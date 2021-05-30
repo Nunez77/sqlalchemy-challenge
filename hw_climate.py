7cf4bd681200555fe121834cfeceafeae28ff3af
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-05-30 00:50:59 -0500

    Cleanup pending

--- hw_climate.py
+++ hw_climate.py
@@ -23,7 +23,7 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Create session
+# Create our session (link) from Python to the DB
 session = Session(engine)
 
 #################################################
@@ -44,38 +44,39 @@ def welcome():
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start><br/>"
-        f"-When given the start date (YYYY-MM-DD), calculates the MIN/AVG/MAX temperature for all dates greater than and equal to the start date<br/>"
         f"/api/v1.0/<start>/<end><br/>"
-        f"- When given the start and the end date (YYYY-MM-DD), calculate the MIN/AVG/MAX temperature for dates between the start and end date inclusive<br/>"
-    )
+            )
 
-#############################################################################################################################################################
+#################################################
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """Returns a list of amounts of precipitation with dates"""
-    # Query all measurements
+
+    # Query all precipitation measurements
     results = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date >= '2016-08-24').filter(Measurement.date <= '2017-08-23').order_by(Measurement.date).all()
+        filter(Measurement.date > '2016-08-23').order_by(Measurement.date).all()
     prcp_dict= {date:prcp for date,prcp in results}
     session.commit()
     time.sleep(2)
 
     # Convert list of tuples into normal list
     return jsonify(prcp_dict)
-###############################################################################################################################################
+
+#################################################
 
 @app.route("/api/v1.0/stations")
 def stations():
     """Returns a list of stations"""
      # Query results to a Dictionary using `date` as the key and `prcp` as the value
-    stations_query = session.query(Station.name, Station.station)
-    stations = pd.read_sql(stations_query.statement, stations_query.session.bind)
+    station_query = session.query(Station.name, Station.station)
+    station = pd.read_sql(station_query.statement, station_query.session.bind)
     session.commit()
     time.sleep(2) 
 
     # Convert list of tuples into normal list
     return jsonify(stations.to_dict())
-#########################################################################################################################################
+#################################################
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -89,7 +90,8 @@ def tobs():
 
     # Convert list of tuples into normal list
     return jsonify(tobs_dict)
-##############################################################################################################################################
+
+#################################################
 
 @app.route("/api/v1.0/<date>")
 def startDateOnly(date):
@@ -100,7 +102,9 @@ def startDateOnly(date):
 
     # Convert list of tuples into normal list
     return jsonify(day_temp_results)
-###########################################################################################################################################################################
+    
+#################################################
+
 @app.route("/api/v1.0/<start>/<end>")
 def startDateEndDate(start,end):
     multi_day_temp_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
